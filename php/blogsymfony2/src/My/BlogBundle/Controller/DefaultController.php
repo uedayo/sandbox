77d07d920529394f4ac98c898ec7d84990e2a840
Yoshinori Ueda <uedayo@gmail.com>
2013-05-28 12:51:48 +0900

    create DefaultController

--- php/blogsymfony2/src/My/BlogBundle/Controller/DefaultController.php
+++ php/blogsymfony2/src/My/BlogBundle/Controller/DefaultController.php
@@ -7,9 +7,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    
-    public function indexAction($name)
+
+    public function indexAction()
     {
-        return $this->render('MyBlogBundle:Default:index.html.twig', array('name' => $name));
+        $em = $this->getDoctrine()->getEntityManager();
+        $posts = $em->getRepository('MyBlogBundle:Post')->findAll();
+        return $this->render('MyBlogBundle:Default:index.html.twig', array('posts' => $posts));
+    }
+
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $post = $em->find('MyBlogBundle:Post', $id);
+        return $this->render('MyBlogBundle:Default:show.html.twig', array('post' => $post));
     }
 }
