2c338f503bb46517456a1de4ed7979ee69602371
Yoshinori Ueda <uedayo@gmail.com>
2013-05-28 16:39:37 +0900

    create edit action

--- php/blogsymfony2/src/My/BlogBundle/Controller/DefaultController.php
+++ php/blogsymfony2/src/My/BlogBundle/Controller/DefaultController.php
@@ -10,7 +10,8 @@ class DefaultController extends Controller
 
     public function indexAction()
     {
-        $em = $this->getDoctrine()->getEntityManager();
+        $em = $this->
+getDoctrine()->getEntityManager();
         $posts = $em->getRepository('MyBlogBundle:Post')->findAll();
         return $this->render('MyBlogBundle:Default:index.html.twig', array('posts' => $posts));
     }
@@ -59,5 +60,39 @@ class DefaultController extends Controller
         $em->remove($post);
         $em->flush();
         return $this->redirect($this->generateUrl('blog_index'));
+    }
+
+    public function editAction($id)
+    {
+        // get data from DB
+        $em = $this->getDoctrine()->getEntityManager();
+        $post = $em->find('MyBlogBundle:Post', $id);
+        if (!$post) {
+            throw new NotFoundHttpException('The post does not exist.');
+        }
+
+        // build form
+        $form = $this->createFormBuilder($post)
+            ->add('title')
+            ->add('body')
+            ->getForm();
+
+        // validate
+        $request = $this->getRequest();
+        if('POST' === $request->getMethod()) {
+            $form->bindRequest($request);
+            if ($form->isValid()) {
+                $post = $form->getData();
+                $post->setUpdatedAt(new \DateTime());
+                $em->flush();
+                return $this->redirect($this->generateUrl('blog_index'));
+            }
+        }
+
+        // draw
+        return $this->render('MyBlogBundle:Default:edit.html.twig', array(
+            'post' => $post,
+            'form' => $form->createView(),
+        ));
     }
 }
