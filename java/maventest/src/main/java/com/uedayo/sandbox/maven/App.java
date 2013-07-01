0dc2fd3e3e307e985b5e0f57729ece4863d9312c
Yoshinori Ueda <uedayo@gmail.com>
2013-07-01 19:03:45 +0900

    add maven sandbox

---
+++ java/maventest/src/main/java/com/uedayo/sandbox/maven/App.java
@@ -0,0 +1,13 @@
+package com.uedayo.sandbox.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
